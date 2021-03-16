e2d1127fe06158c6dcddc093b58277aa61fad856
Howe Robbin <hrobbin@et-d6.kzn.21-school.ru>
2021-03-16 19:34:24 +0300

    final[3]

--- headers/header.h
+++ headers/header.h
@@ -32,22 +32,14 @@
 # include "calc_light.h"
 # include "utils.h"
 # ifdef __linux__
-#  define OS "Linux"
 #  define ESC 65307
 #  define TAB 65289
 #  define M_PI 3.14159265358979323846
 # elif __APPLE__
-#  define OS "Mac"
 #  define ESC 53
 #  define TAB 48
 # endif
 # define RED   "\x1B[31m"
-# define GRN   "\x1B[32m"
-# define YEL   "\x1B[33m"
-# define BLU   "\x1B[34m"
-# define MAG   "\x1B[35m"
-# define CYN   "\x1B[36m"
-# define WHT   "\x1B[37m"
 # define RESET "\x1B[0m"
 # define MFAIL "Malloc failed"
 # define BG_COLOUR 0x48494B
